456a3b71cfa11ccd06c586ff130c043988f06f84
Oskar Pliszczak <116510728+Oskar-pli@users.noreply.github.com>
2026-08-26 23:51:53 +0200

    Upgrade service worker to version 2 with improvements

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
-const CACHE_NAME = 'party-lab-v1';
+const CACHE_NAME = "party-lab-v2";
 
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -26,12 +26,24 @@ self.addEventListener('activate', event => {
       )
     )
   );
+
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
